6d78753b838ca4dd3da75afa82a0977a53a9cc0d
Adriel Itamar <adrielitamar@gmail.com>
2023-10-21 11:19:20 -0300

    feat: add route protection

--- src/hooks/useKeepUser.ts
+++ src/hooks/useKeepUser.ts
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import Cookies from "js-cookie";
 
 export function useKeepUser() {
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user) {
+    const token = Cookies.get('token');
+    if (!user || !token) {
       const recoverData = JSON.parse(sessionStorage.getItem("user") || "{}");
       if (Object.keys(recoverData).length) {
         setUser(recoverData);
