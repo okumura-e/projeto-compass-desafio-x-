eac8ccd386ab3021ae4084a5f591777715ae8402
Adriel Itamar <adrielitamar@gmail.com>
2023-10-24 10:32:12 -0300

    feat: add token validation

--- src/hooks/useKeepUser.ts
+++ src/hooks/useKeepUser.ts
@@ -1,14 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { UserContext } from "../context/UserContext";
 import Cookies from "js-cookie";
 
 export function useKeepUser() {
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
+  const [path, setPath] = useState(window.location.pathname);
 
   useEffect(() => {
     const token = Cookies.get('token');
+    setPath(window.location.pathname);
     if (!user) {
       const recoverData = JSON.parse(sessionStorage.getItem("user") || "{}");
       if (Object.keys(recoverData).length) {
@@ -17,7 +19,10 @@ export function useKeepUser() {
         navigate("/");
       }
     }
-  }, [user]);
+    if (!token && path !== '/new-password') {
+      navigate("/");
+    }
+  }, [user, path]);
 
   return { user, setUser, navigate };
 }
